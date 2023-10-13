c5690c34bef7fcf32d5a82c8662e9d36f514160b
rajironman <128172464+rajironman@users.noreply.github.com>
2023-10-13 21:16:50 +0530

    Delete erm_api directory

--- erm_api/authentication/set_env.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-import string
-
-n = 20
-
-random_string = "".join(random.choices(string.ascii_letters+string.digits,k=n))
-# os.environ['secret_key'] = str(random_string)
-print(os.environ['secret_key'])
